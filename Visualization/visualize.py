bba5a98fd4d9864f48016c81e3978c12ae0d1105
MathiasSagerup <mathias.sagerup@gmail.com>
2024-08-26 19:31:12 +0200

    f

--- Visualization/visualize.py
+++ Visualization/visualize.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from utils.loader import load_data
 
-def correlation_matrix_make_png(data: pd.DataFrame, fileName: str) -> None:
+def correlation_matrix_make_png(fileName: str, data_config: dict) -> None: 
+    data = load_data(data_config["data"]["raw"])
     correlation_matrix = data.corr()
     plt.figure(figsize=(10, 10))
     sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', linewidths=0.5)
-    plt.savefig(f'../output/{fileName}', format='png', dpi=300, bbox_inches='tight')
-    plt.show()
+    saveFolder = data_config["Output"]["correlation_matrix"]
+    print(saveFolder)
+    plt.savefig(saveFolder, format='png', dpi=300, bbox_inches='tight')
 
